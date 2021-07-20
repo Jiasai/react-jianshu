98e29c79f718a837d27d19cdabcca198e4e54e86
Zhang JiaSai <1556009335@qq.com>
2021-07-20 11:58:22 +0800

    react语法示例

--- src/view/home/hook/reactDemo.js
+++ src/view/home/hook/reactDemo.js
@@ -1,20 +1,19 @@
-
 import axios from "axios";
-import React, { useState, useEffect, Fragment } from "react";
-import { Link } from "react-router-dom";
-import Topic from "../components/Topic";
-import "./hello.css";
+import React, { useState, useEffect, Fragment } from "react"; //引入react内置API方法
+import { Link } from "react-router-dom";                     //react-router路由跳转
+import Topic from "../components/Topic";                    //引入使用的组件
+import "./hello.css";                                       //写样式
 
-const Hello = (props) => {
-    const [username, setUsername] = useState('');
-    const [isActive,] = useState(true);
+const Hello = (props) => {                                 // 定义 函数组件 
+    const [username, setUsername] = useState('');          // 定义响应式变量
+    const [isActive,] = useState(true);             
     const [isUser, setIsUser] = useState(true);
 
-    const getIsUser = async () => {
+    const handleGetIsUser = async () => {                       //定义一个发送请求函数
         const res = await axios.get(`/api/user/${username}`);
         if (res?.errno === 0) {
-            setIsUser(res.data.isUser)
-            setTimeout(() => { props.history.push(`/api/user/${username}`) }, 2000)
+            setIsUser(res.data.isUser)   //修改响应式变量的值
+            setTimeout(() => { props.history.push(`/detail/${username}`) }, 2000) //路由跳转
         }
     }
 
@@ -24,24 +23,28 @@ const Hello = (props) => {
         console.log(username, "username发生变化，useEffect重新执行");
     }, [username])
 
-    return (
-        <Fragment>
+    return ( // return Dom结构
+        <Fragment>                              {/*占位符,jsx中，注释要这么写*/}
             <div>Hello,world</div>
             <p>This is React！</p>
-            <Link to={`/user/${username}`}>
+            <Link to={`/user/${username}`}>     {/*js变量和语法写在 {} 中 */}
                 去主页链接
-            </Link>
-            <h1 className={isActive ? "active" : ""}>{props.pageTitle}</h1>
+            </Link>                                                          {/*props 接收组件的属性传值*/}
+            <h1 className={isActive ? "active" : ""}>{props.pageTitle}</h1>  {/*Dom中文本变量放在 {} 中 */}
+                                                   {/*通过onChange修改input的value值,username是响应式变量*/}
             <input type="text"
                 value={username}
-                onChange={(e) => { setUsername(e.target.value) }}
+                onChange={(e) => { setUsername(e.target.value) }}  
                 style={{ backgroundColor: "#eee" }}
             />
+            {/*style样式要写在 {} 中，写成对象形式 */}
             {
                 isUser ? <span>用户名正确</span> : <span>用户名不正确，重新输入</span>
             }
-            <button onClick={getIsUser}>验证</button>
-            <ul>
+            {/*通过三元运算符，确定返回Dom的节点 */}
+            {/*on关键字绑定事件：onClick、onChange、onFocus、onBlur、onMouseEnter */}
+            <button onClick={handleGetIsUser}>验证</button>
+            <ul>                                {/*.map()方法实现循环输出，vue中使用v-for指令*/}
                 {
                     props.ListData.map((item, index) => {
                         return (
@@ -50,7 +53,7 @@ const Hello = (props) => {
                     })
                 }
             </ul>
-            <Topic pageTitle={props.pageTitle} />
+            <Topic pageTitle={props.pageTitle} />   {/*组件属性传值*/}
         </Fragment>
     )
 }
